1db6d4f7f454ce3d68ade22ee2edda57081810ab
Michal Ostrowski <michal.ostrowski@forlagshuset.no>
2021-06-01 08:03:56 +0200

    fix: expose sort validation error

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export {
 } from './output.dto';
 export { CollectionProperties, CollectionPropertyOptions } from './property';
 export { FilterValidationError } from './filter/validation.error';
+export { SortValidationError } from './sorter/validation.error';
 export { DocumentCollector } from './executor/collector';
 export { ValidationPipe } from './validation.pipe';
 export { Expose } from './property.decorator';
