ef27d41b4300e324191483c361b6526b092c9111
Rafal Wrzochol <rafal.wrzochol@gmail.com>
2022-04-07 17:18:01 +0200

    refactor: add validation and missing tests for input dto's

---
+++ src/input.dto.spec.ts
@@ -0,0 +1,108 @@
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { CollectionDto, CounterDto } from './input.dto';
+
+describe('CounterDto', () => {
+  it('should transform filter attribute into object', async () => {
+    const payload = {
+      filter: '{"userName": {"$regex": "^test"}}',
+    };
+    const counterDto = plainToClass(CounterDto, payload);
+    const errors = await validate(counterDto);
+    expect(errors.length).toBe(0);
+  });
+});
+
+describe('CollectionDto', () => {
+  it('should transform filter attribute into object', async () => {
+    const payload = {
+      filter: '{"userName": {"$regex": "^test"}}',
+    };
+    const collectionDto = plainToClass(CollectionDto, payload);
+    const errors = await validate(collectionDto);
+    expect(errors.length).toBe(0);
+  });
+
+  it('should allow only string type for sort attribute', async () => {
+    const payload = {
+      sort: '-createdAt;filename',
+    };
+    const collectionDto = plainToClass(CollectionDto, payload);
+    const errors = await validate(collectionDto);
+    expect(errors.length).toBe(0);
+  });
+
+  describe('page attribute', () => {
+    it('should allow number greater then zero', async () => {
+      const payload = {
+        page: 1,
+      };
+      const collectionDto = plainToClass(CollectionDto, payload);
+      const errors = await validate(collectionDto);
+      expect(errors.length).toBe(0);
+    });
+
+    it('should allow number equal then zero', async () => {
+      const payload = {
+        page: 0,
+      };
+      const collectionDto = plainToClass(CollectionDto, payload);
+      const errors = await validate(collectionDto);
+      expect(errors.length).toBe(0);
+    });
+
+    it('should not allow number less then zero', async () => {
+      const payload = {
+        page: -1,
+      };
+      const collectionDto = plainToClass(CollectionDto, payload);
+      const errors = await validate(collectionDto);
+      expect(errors.length).not.toBe(0);
+    });
+
+    it('should default to 0 if not given', async () => {
+      const payload = {};
+      const collectionDto = plainToClass(CollectionDto, payload);
+      const errors = await validate(collectionDto);
+      expect(errors.length).toBe(0);
+      expect(collectionDto.page).toBe(0);
+    });
+  });
+
+  describe('limit attribute', () => {
+    it('should allow number greater then zero', async () => {
+      const payload = {
+        limit: 1,
+      };
+      const collectionDto = plainToClass(CollectionDto, payload);
+      const errors = await validate(collectionDto);
+      expect(errors.length).toBe(0);
+    });
+
+    it('should allow number equal zero', async () => {
+      const payload = {
+        limit: 0,
+      };
+      const collectionDto = plainToClass(CollectionDto, payload);
+      const errors = await validate(collectionDto);
+      expect(errors.length).toBe(0);
+    });
+
+    it('should not allow number less then zero', async () => {
+      const payload = {
+        limit: -1,
+      };
+      const collectionDto = plainToClass(CollectionDto, payload);
+      const errors = await validate(collectionDto);
+      expect(errors.length).not.toBe(0);
+    });
+
+    it('should default to 10 if not given', async () => {
+      const payload = {};
+      const collectionDto = plainToClass(CollectionDto, payload);
+      const errors = await validate(collectionDto);
+      expect(errors.length).toBe(0);
+      expect(collectionDto.limit).toBe(10);
+    });
+  });
+});
